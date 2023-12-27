9662ae2913ab7aa6fb32be7c7ebd0f8b2ceb80f7
cojovi <cojovi.mma@gmail.com>
2023-12-27 12:41:53 -0600

    Updated Firebase version and added actual configuration
    Firebase dependency has been upgraded to the latest version to access the newest features and improvements. Additionally, the Firebase configuration placeholders in `util/firebaseConfig.ts` have been replaced with actual project-specific settings, preparing the application for Firebase integration.
    
    This update is a step towards enabling Firebase functionalities, ensuring that the application remains up-to-date with Firebase's API changes and performance enhancements. It's important to verify that the new configuration aligns with the project's Firebase setup and that any new features or breaking changes introduced in the Firebase update are accommodated within the application.

--- util/firbaseConfig.ts
+++ util/firbaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'place your FIREBASE config here',
-  authDomain: 'place your FIREBASE config here',
-  projectId: 'place your FIREBASE config here',
-  storageBucket: 'place your FIREBASE config here',
-  messagingSenderId: 'place your FIREBASE config here',
-  appId: 'place your FIREBASE config here',
+  apiKey: "AIzaSyAl_yDNuHIQOvpiiWuVoqAP3OOxXzSdoAY",
+  authDomain: "reel-unravel.firebaseapp.com",
+  projectId: "reel-unravel",
+  storageBucket: "reel-unravel.appspot.com",
+  messagingSenderId: "243377426943",
+  appId: "1:243377426943:web:dedbe928830d4197bed7ca",
 };
 
 const app = initializeApp(firebaseConfig);
